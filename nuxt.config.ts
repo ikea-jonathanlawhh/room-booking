60fefc961d955f6019c7cd9b7159ad906e0e46c7
Jonathan Hui Hao  Law <jonathanhuihao,law@inter.ikea.com>
2026-08-13 16:16:24 +0800

    fix(db): disable database migration during build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   hub: {
     db: {
       dialect: 'mysql',
+      applyMigrationsDuringBuild: false,
       connection: {
         ssl: {
           rejectUnauthorized: false
